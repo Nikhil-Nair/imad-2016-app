b44a95825c011605a0c131a3ab4a272f0acc97f8
Nikhil-Nair <nikhil.nair.2793@gmail.com>
2016-10-17 23:39:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,9 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'New-Value';
+
+
+//Moving Madi!!!!
+
+var img = document.getElementById('madi');
+img.style.marginLeft = '100px';
